33d1b40a75d3ab2d56ec230c8ab8d07442eabd53
Siddharth Shah <sas2387@columbia.edu>
2017-02-02 20:21:19 -0500

    Added code for DFS

--- Assignment1/driver.py
+++ Assignment1/driver.py
@@ -3,6 +3,11 @@ import math
 from collections import deque
 from sets import Set
 import copy
+import datetime
+import resource
+
+start_time = None
+end_time = None
 
 def goalTest(state):
 	dimen = len(state.board)
@@ -20,7 +25,6 @@ def print_board(state):
 		for j in range(dimen):
 			print state.board[i][j]," ",
 		print
-	print 
 
 
 class State(object):
@@ -38,7 +42,21 @@ class State(object):
 				index = index + 1
 
 	def __str__(self):
-         return str(self.board)
+		return str(self.board)
+	"""
+	def __key(self):
+		l = list()
+		for line in self.board:
+			for ele in line:
+				l.append(ele)
+		return tuple(l)
+		#return str(self.board)
+
+	def __eq__(x, y):
+		return x.__key() == y.__key()
+
+	def __hash__(self):
+		return hash(self.__key())"""
 
 	def getUpNeighbour(self):
 		if self.posX > 0:
@@ -94,18 +112,30 @@ def performBFS(initialState,goalTest):
 	explored = Set()
 	result = False
 	answer = None
-	maxQueueCount = 0
+	fringe_size = 0
+	max_fringe_size = 0
+	nodesExpanded = 0
+	search_depth = 0
+	max_search_depth = 0
+	max_ram_usage = 0
 	frontier.append(initialState)
+	usage = resource.getrusage(resource.RUSAGE_SELF)
+	if usage > max_ram_usage:
+		max_ram_usage = usage
+	if max_fringe_size < len(frontier):
+		max_fringe_size = len(frontier)
 
 	while len(frontier) != 0 :
 		state = frontier.popleft()
-		print_board(state)
-		maxQueueCount = maxQueueCount + 1
 		explored.add(state)
+		usage = resource.getrusage(resource.RUSAGE_SELF)
+		if usage > max_ram_usage:
+			max_ram_usage = usage
 
 		if(goalTest(state)):
 			result = True
 			answer = state
+			fringe_size = len(frontier)
 			break
 
 		upNeighbour = state.getUpNeighbour()
@@ -113,33 +143,133 @@ def performBFS(initialState,goalTest):
 		leftNeighbour = state.getLeftNeighbour()
 		rightNeighbour = state.getRightNeighbour()
 
-		#print "Up: "+str(upNeighbour)
-		#print "Down: "+str(downNeighbour)
-		#print "Left: "+str(leftNeighbour)
-		#print "Right: "+str(rightNeighbour)
+		nodesExpanded = nodesExpanded + 1
 
 		if upNeighbour != None:
+			if max_search_depth < len(upNeighbour.operations):
+				max_search_depth = len(upNeighbour.operations)
 			if frontier.count(upNeighbour) == 0 and upNeighbour not in explored :
 				frontier.append(upNeighbour)
 
 		if downNeighbour != None:
+			if max_search_depth < len(downNeighbour.operations):
+				max_search_depth = len(downNeighbour.operations)
 			if frontier.count(downNeighbour) == 0 and downNeighbour not in explored :
 				frontier.append(downNeighbour)
 
 		if leftNeighbour != None:
+			if max_search_depth < len(leftNeighbour.operations):
+				max_search_depth = len(leftNeighbour.operations)
 			if frontier.count(leftNeighbour) == 0 and leftNeighbour not in explored :
 				frontier.append(leftNeighbour)
 
 		if rightNeighbour != None:
+			if max_search_depth < len(rightNeighbour.operations):
+				max_search_depth = len(rightNeighbour.operations)
 			if frontier.count(rightNeighbour) == 0 and rightNeighbour not in explored :
 				frontier.append(rightNeighbour)
 
+		if max_fringe_size < len(frontier):
+			max_fringe_size = len(frontier)
+
+		usage = resource.getrusage(resource.RUSAGE_SELF)
+		if usage > max_ram_usage:
+			max_ram_usage = usage
+
 	if result:
 		#print state
-		print "path_to_goal: ",state.operations
-		print "cost_of_path: ",len(state.operations)
-		print explored
-		print maxQueueCount
+		print "path_to_goal:", state.operations
+		print "nodes_expanded:", nodesExpanded
+		print "cost_of_path:", len(state.operations)
+		print "fringe_size:", fringe_size
+		print "max_fringe_size:", max_fringe_size
+		print "search_depth:", len(state.operations)
+		print "max_search_depth:", max_search_depth
+		end_time = datetime.datetime.now()
+		print "running_time: %.8f" %(end_time - start_time).total_seconds()
+		print "max_ram_usage:", max_ram_usage.ru_maxrss / (1024 * 1024)
+
+def performDFS(initialState,goalTest):
+	frontier = list()
+	explored = Set()
+	result = False
+	answer = None
+	fringe_size = 0
+	max_fringe_size = 0
+	nodesExpanded = 0
+	search_depth = 0
+	max_search_depth = 0
+	max_ram_usage = 0
+	frontier.append(initialState)
+	usage = resource.getrusage(resource.RUSAGE_SELF)
+	if usage > max_ram_usage:
+		max_ram_usage = usage
+	if max_fringe_size < len(frontier):
+		max_fringe_size = len(frontier)
+
+	while len(frontier) != 0 :
+		state = frontier.pop()
+		explored.add(state)
+		print nodesExpanded
+		usage = resource.getrusage(resource.RUSAGE_SELF)
+		if usage > max_ram_usage:
+			max_ram_usage = usage
+
+		if(goalTest(state)):
+			result = True
+			answer = state
+			fringe_size = len(frontier)
+			break
+
+		upNeighbour = state.getUpNeighbour()
+		downNeighbour = state.getDownNeighbour()
+		leftNeighbour = state.getLeftNeighbour()
+		rightNeighbour = state.getRightNeighbour()
+
+		nodesExpanded = nodesExpanded + 1
+
+		if rightNeighbour != None:
+			if max_search_depth < len(rightNeighbour.operations):
+				max_search_depth = len(rightNeighbour.operations)
+			if frontier.count(rightNeighbour) == 0 and rightNeighbour not in explored :
+				frontier.append(rightNeighbour)
+
+		if leftNeighbour != None:
+			if max_search_depth < len(leftNeighbour.operations):
+				max_search_depth = len(leftNeighbour.operations)
+			if frontier.count(leftNeighbour) == 0 and leftNeighbour not in explored :
+				frontier.append(leftNeighbour)
+
+		if downNeighbour != None:
+			if max_search_depth < len(downNeighbour.operations):
+				max_search_depth = len(downNeighbour.operations)
+			if frontier.count(downNeighbour) == 0 and downNeighbour not in explored :
+				frontier.append(downNeighbour)
+
+		if upNeighbour != None:
+			if max_search_depth < len(upNeighbour.operations):
+				max_search_depth = len(upNeighbour.operations)
+			if frontier.count(upNeighbour) == 0 and upNeighbour not in explored :
+				frontier.append(upNeighbour)		
+
+		if max_fringe_size < len(frontier):
+			max_fringe_size = len(frontier)
+
+		usage = resource.getrusage(resource.RUSAGE_SELF)
+		if usage > max_ram_usage:
+			max_ram_usage = usage
+
+	if result:
+		print "path_to_goal:", state.operations
+		print "cost_of_path:", len(state.operations)
+		print "nodes_expanded:", nodesExpanded
+		print "fringe_size:", fringe_size
+		print "max_fringe_size:", max_fringe_size
+		print "search_depth:", len(state.operations)
+		print "max_search_depth:", max_search_depth
+		end_time = datetime.datetime.now()
+		print "running_time: %.8f" %(end_time - start_time).total_seconds()
+		print "max_ram_usage:", max_ram_usage.ru_maxrss / (1024 * 1024)
 
 
 if len(sys.argv) == 3 :
@@ -149,13 +279,14 @@ if len(sys.argv) == 3 :
 	if math.pow(dimen, 2) != len(grid):
 		print "Input size of board is not correct"
 		exit()
+	start_time = datetime.datetime.now()
 	initialState = State(grid, dimen)
 	if type == "bfs":
 		# Perform BFS
 		performBFS(initialState, goalTest)
 	elif type == "dfs":
 		# Perform DFS
-		print "Performing DFS"
+		performDFS(initialState, goalTest)
 	elif type == "ast":
 		# Perform AST
 		print "Perform A-Star Search"
